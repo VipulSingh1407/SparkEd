c25ab0ba4e88df6c0e32504d8069f8ec350ac7c8
VipulSingh1407 <vipulsingh9369384252@gmail.com>
2024-01-17 22:19:14 +0530

    Update ResetPassword.js

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -33,7 +33,7 @@ exports.resetPasswordToken = async (req, res) => {
         //link generation...
         //create url
         // const url = `http://localhost:3000/update-password/${token}`;
-        const url = `https://sparked-frontend.vercel.app//update-password/${token}`;
+        const url = `https://sparkedvamd.vercel.app//update-password/${token}`;
 
         //sending... mail
         await mailSender(email, 
