155bb5cf698d2678d728a8027cd07b445d99b61c
VipulSingh1407 <vipulsingh9369384252@gmail.com>
2024-01-18 14:35:18 +0530

    Update mailSender.js

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -12,7 +12,7 @@ const mailSender = async (email, title, body) => {
         })
 
         let info = await transporter.sendMail({
-            from: 'SparkEd ||  by Team',
+            from: 'SparkEd-Team',
             to:`${email}`,
             subject: `${title}`,
             html: `${body}`,
