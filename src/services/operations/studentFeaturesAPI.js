63247396343d377edb00a1168c3ecc62c3ce29eb
VipulSingh1407 <vipulsingh9369384252@gmail.com>
2024-01-20 16:08:20 +0530

    update

--- src/services/operations/studentFeaturesAPI.js
+++ src/services/operations/studentFeaturesAPI.js
@@ -1,7 +1,7 @@
 import { toast } from "react-hot-toast";
 import { studentEndpoints } from "../apis";
 import { apiConnector } from "../apiconnector";
-import rzpLogo from "../../assets/Logo/email logo.png"
+import rzpLogo from "../../assets/Logo/emaillogo.png"
 import { setPaymentLoading } from "../../slices/courseSlice";
 import { resetCart } from "../../slices/cartSlice";
 
